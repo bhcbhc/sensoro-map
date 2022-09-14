98c6e260d5be26b6a3d8afca0c03a7a56caebe00
bhcbhc <ninghai@sensoro.com>
2022-09-14 17:53:59 +0800

    docs:修改demo示例

--- docs/position-selector/demo/demo-05.tsx
+++ docs/position-selector/demo/demo-05.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Icon from '@sensoro/sensoro-design/es/icon';
 import { PositionSelector } from '@sensoro/sensoro-map';
 
 export default () => {
+  const [val, setVal] = useState({ lnglat: [116.905163, 40.006047], location: '这个是一个位置' });
   return (
     <PositionSelector
       style={{ width: 550, height: 300 }}
-      value={{ lnglat: [116.905163, 40.006047], location: '这个是一个位置' }}
+      value={val}
+      onChange={setVal}
       icon={<Icon type="icon-car-outlined" style={{ fontSize: 24, color: 'red' }} />}
     />
   );
